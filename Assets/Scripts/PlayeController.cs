f94849ec95ee9556f489ffaa6bbc79e87addb6bd
Paul Thibaul <Paulthb1@gmail.com>
2020-06-19 17:51:38 +0200

    add move and gameOver

--- Assets/Scripts/PlayeController.cs
+++ Assets/Scripts/PlayeController.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class PlayeController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D rb;
+
+    private float t = 0.0f;
+    private bool moving = false;
+
+    [SerializeField]
+    private string horizontalAxe = "";
+    [SerializeField]
+    private string verticalAxe = "";
+
+    public float speed = 5;
+
+    public GameObject gameOverText = null;
+
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //plus tard
+        float moveHorizontal = Input.GetAxis(horizontalAxe);
+        float moveVertical = Input.GetAxis(verticalAxe);
+        Vector2 movement = new Vector3(moveHorizontal * speed, moveVertical * speed);
+
+        rb.velocity = new Vector2(movement.x, movement.y);
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity = rb.velocity * 0.9f;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //si on sot du citron, Game Over
+        if(collision.gameObject.tag == "Platform")
+        {
+            gameOverText.SetActive(true);
+        }
     }
 }
