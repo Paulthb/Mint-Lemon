1ae7d888358a192423f8f99f647f970eb538fb47
Paul Thibaul <Paulthb1@gmail.com>
2020-06-22 15:21:23 +0200

    add MrMint Anim for valentine

---
+++ Assets/Scripts/AcidDrop.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcidDrop : MonoBehaviour
+{
+    private SpriteRenderer acidDroppedSprite = null;
+
+    private CircleCollider2D col = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<CircleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /**
+     * quand l'object atteint sa position
+     * on active son collider et on change son sprite
+     */
+    public void AcidDropped()
+    {
+        col.enabled = true;
+    }
+}
---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<Transform> spawnerPosList = null;
+
+    [SerializeField]
+    private List<Transform> acidDropPosList = null;
+
+    [SerializeField]
+    private List<GameObject> AcidDropList = null;
+
+    #region Singleton Pattern
+    private static EventManager _instance;
+
+    public static EventManager Instance { get { return _instance; } }
+    #endregion
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+            Destroy(this.gameObject);
+
+        else
+            _instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            AcidDrop();
+    }
+
+    public void AcidDrop()
+    {
+        int randSpawnerId = Random.Range(0, spawnerPosList.Count);
+        int randAcidDropObject = Random.Range(0, 0);//pour l'instant
+
+
+    }
+}
